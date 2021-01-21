ed89ca90220cde9488e9f0bfc82f2c54920acdf7
Zach Murray <Zachary.a.murray@hotmail.com>
2021-01-20 18:06:45 -0600

    Completed Menu

--- Wk3Grp01Topic09LAB/main.cpp
+++ Wk3Grp01Topic09LAB/main.cpp
@@ -22,11 +22,18 @@ Steps
 #include <iostream>
 #include <string>
 #include <conio.h> //_getch()
+#include <vector> // vector (array type) for discovered primes
 using std::string;
 using std::cout;
 using std::cin;
 using std::endl;
+using std::vector;
 
+//Variables
+int maxSize = 1000;//Limiter for prime checking to avoid crash
+vector<int> P;
+
+//Function Prototypes
 bool isEven(int i);//check if number divides by 2
 bool isPrime(int i);// check in number is a multiple of a lower prime
 void cyclePrimes(void);// check each real number for prime until user stops
@@ -34,7 +41,6 @@ void cyclePrimes(void);// check each real number for prime until user stops
 int main(void)
 {
     int n;//variable to input array
-    int* P = new int(n);//Dynamic array for discovered primes
 
     int choice;//Menu response
     do
@@ -50,23 +56,29 @@ int main(void)
 
         switch (choice)//Choice decision branch
         {
-        case 1:
+        case 1://Check a single number for prime
             int aNum = 0;
             cout << "Please enter the number you would like to check: " << endl;
             cin >> aNum;
             if (isdigit(aNum) && (aNum % 2 != 0))//is it a whole number? Can it be divided evenly?
-                    isPrime(aNum);//is it divisible by another prime?
+                isPrime(aNum);//is it divisible by another prime?
+            else
+                cout << aNum << "is not a prime number";
+
             break;
-        case 2:
+        case 2://Check for primes up to maxSize
             cout << "Checking all real whole numbers for primes..." << endl;
-            cyclePrimes();
+            P.clear();//Clear existing list to avoid relisting discovered numbers.
+            cyclePrimes();//Checking for prime numbers
 
             break;
-        case 3:
+        case 3://Display known primes
+            cout << "Discovered Primes are:\n\n";
+            for (auto i = P.begin(); i != P.end(); ++i)//Output Iterator of Vector
+                cout << *i << "\n";
 
             break;
-        case 4:
-
+        case 4://Exit program
             break;
         default:
             cout << "That is not a valid choice." << endl;
@@ -74,14 +86,9 @@ int main(void)
             _getch();
             break;
         }
-    } while (choice != 4);
+    } while (choice != 4);//Exit if 4 is selected
 
 	return 0;
-}
-
-bool isEven(int i)
-{
-   if (i % 2) return 
 }
 
 bool isPrime(int i)
@@ -91,7 +98,7 @@ bool isPrime(int i)
 
 void cyclePrimes(void)
 {
-    for (int n = 0; n < 1000 - 1; n++)
+    for (int i = 0; i < maxSize - 1; i++)
     {
         if (isdigit(n) && (n % 2 != 0))//is it a whole number? Can it be divided evenly?
             if (isPrime(n))
